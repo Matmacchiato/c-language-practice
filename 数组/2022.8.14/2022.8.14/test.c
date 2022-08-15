25c1516f2a7883ff65d328a2c2fef6920ac62aaa
matmacciato <1362478331@qq.com>
2022-08-15 21:26:25 +0800

    test

--- 数组/2022.8.14/2022.8.14/test.c
+++ 数组/2022.8.14/2022.8.14/test.c
@@ -29,31 +29,39 @@
 //}
 
 // 冒泡排序
-void bubble_sort(int arr[], int sz)
-{
-	// 确定躺数
-	int i = 0;
-	for (i = 0; i < sz - 1; i++)
-	{
-		// 一趟冒泡排序的过程
-		int j = 0;
-		for (j = 0; j < sz - 1 - i; j++)
-		{
-			if (arr[j] > arr[j + 1]) {
-				int tmp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = tmp;
-			}
-		}
-	}
-}
+//void bubble_sort(int arr[], int sz)
+//{
+//	// 确定躺数
+//	int i = 0;
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		// 一趟冒泡排序的过程
+//		int j = 0;
+//		for (j = 0; j < sz - 1 - i; j++)
+//		{
+//			if (arr[j] > arr[j + 1]) {
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}
+//}
 
 int main()
 {
-	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
-	// 改为升序排序
-	// 计算数组元素个数
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	bubble_sort(arr, sz);
+	//int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
+	//// 改为升序排序
+	//// 计算数组元素个数
+	//int sz = sizeof(arr) / sizeof(arr[0]);
+	//bubble_sort(arr, sz);
+	//int a = 732;
+	//printf("%d\n", a);
+	//printf("%-10d\n", a);
+
+	int b;
+	float c;
+	scanf("%2d%f", &b, &c);
+	printf("%2d\n%f", b, c);
 	return 0;
 }
